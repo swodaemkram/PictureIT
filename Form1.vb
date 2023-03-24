890c51174440b06ede8b873084148a983abfb20d
mark <mark@MARKSLAPTOP>
2023-03-24 18:29:42 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Form2.MdiParent = Me
+        Form2.Show()
+
+
+    End Sub
+End Class
