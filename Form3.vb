65216c0c0b320f1ced4ec51e8812a4292cebcaec
swodaemkram <swodaemkram@gmail.com>
2023-04-14 08:52:43 -0400

    Todays Update 04142023

--- Form3.vb
+++ Form3.vb
@@ -18,10 +18,23 @@ THEVERYTOP:
 
         If BackgroundWorker1.CancellationPending = True Then Exit Sub
 
+
+
         Dim ping As New System.Net.NetworkInformation.Ping
-        Dim ms = ping.Send("10.10.2.1").RoundtripTime()
+        'Dim ms = ping.Send("10.10.2.1").RoundtripTime()
+        Dim ms = ping.Send(DataGridView1.Rows.Item(0).Cells(3).Value).RoundtripTime()
+
+        DataGridView1.Rows.Item(0).Cells(5).Value = ms
+        DataGridView1.Rows.Item(0).Cells(7).Value = DataGridView1.Rows.Item(0).Cells(7).Value + 1
+
+        ms = ping.Send(DataGridView1.Rows.Item(0).Cells(4).Value).RoundtripTime()
         DataGridView1.Rows.Item(0).Cells(6).Value = ms
+        DataGridView1.Rows.Item(0).Cells(8).Value = DataGridView1.Rows.Item(0).Cells(8).Value + 1
+
+        'DataGridView1.Rows.Item(0).Cells(9).Value = DataGridView1.Rows.Item(9).Cells(7).Value + 1
+
 
         GoTo THEVERYTOP
+
     End Sub
 End Class
